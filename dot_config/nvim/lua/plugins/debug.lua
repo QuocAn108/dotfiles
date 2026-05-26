84ca3152da530720867283b3fef55852591ebcd1
QuocAn108 <anhoang800140@gmail.com>
2026-05-26 23:53:46 +0700

    change shortcut dapUI

--- dot_config/nvim/lua/plugins/debug.lua
+++ dot_config/nvim/lua/plugins/debug.lua
@@ -27,28 +27,28 @@ return {
 	keys = {
 		-- Basic debugging keymaps, feel free to change to your liking!
 		{
-			"<F5>",
+			"<leader><F5>",
 			function()
 				require("dap").continue()
 			end,
 			desc = "Debug: Start/Continue",
 		},
 		{
-			"<F1>",
+			"<leader><F1>",
 			function()
 				require("dap").step_into()
 			end,
 			desc = "Debug: Step Into",
 		},
 		{
-			"<F2>",
+			"<leader><F2>",
 			function()
 				require("dap").step_over()
 			end,
 			desc = "Debug: Step Over",
 		},
 		{
-			"<F3>",
+			"<leader><F3>",
 			function()
 				require("dap").step_out()
 			end,
@@ -70,7 +70,7 @@ return {
 		},
 		-- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
 		{
-			"<F7>",
+			"<leader><F7>",
 			function()
 				require("dapui").toggle()
 			end,
